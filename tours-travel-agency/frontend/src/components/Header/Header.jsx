20d1cb79a9b10019dd33b7c5da7a743ec0479f41
arif1101 <arifurrahmanarif223@gmail.com>
2024-04-23 20:17:09 +0600

    commit from arif

--- tours-travel-agency/frontend/src/components/Header/Header.jsx
+++ tours-travel-agency/frontend/src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import {Container, Row, Button} from 'reactstrap';
-import {NavLink, Link} from 'react-router-dom';
+import React, {useRef, useEffect, useContext} from 'react'  
+import { Container, Row, Button} from 'reactstrap'
+import { NavLink, Link, useNavigate} from 'react-router-dom'
 
-import logo from '../../assets/images/logo.png';
-import './header.css';
+import logo from '../../assets/images/logo.png'
+import "./header.css";
+
+import {AuthContext} from './../../context/AuthContext'
+
 
 const nav__links=[
   {
@@ -11,7 +14,7 @@ const nav__links=[
     display:'Home'
   },
   {
-    path:'/about',
+    path:'about',
     display:'About'
   },
   {
@@ -26,55 +29,105 @@ const nav__links=[
     path:'/flight',
     display:'Flight'
   }
+];
 
-]
 
-const header = () => {
-  return <header className="header">
+const Header = () => {
+
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+  const navigate = useNavigate()
+  const {user, dispatch} = useContext(AuthContext)
+
+  const logout = ()=>{
+    dispatch({type:'LOGOUT'})
+    navigate('/')
+  }
+
+  const stickyHeaderFunc = () =>{
+    window.addEventListener('scroll', ()=>{
+      if(document.body.scrollTop >80 || document.documentElement.scrollTop >80){
+        headerRef.current.classList.add('sticky__header')
+      }else{
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(()=>{
+    stickyHeaderFunc()
+
+    return window.removeEventListener('scroll', stickyHeaderFunc)
+  })
+
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu')
+
+  return <header className="header" ref={headerRef}>
     <Container>
       <Row>
-        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+        <div className="nav__wrapper d-flex align-items-center
+        justify-content-between">
 
-          {/* =========== logo ============*/}
+          {/*========= logo ========= */}
           <div className="logo">
             <img src={logo} alt="" />
           </div>
-          {/* =========== logo end ============*/}
+          {/* ========== logo end =========== */}
 
-          {/* =========== menu start ============*/}
-          <div className="navigation">
+          {/* =t=r=a=v=e=l= Menu Start =t=o=u=r=  */}
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu d-flex align-items-center gap-5">
-              {nav__links.map((item,index)=>(
-                <li className="nav__item" key={index}>
-                  <NavLink to={item.path}
-                  className={navClass =>
-                    navClass.isActive ? "active__link" : ""
-                  }
-                  >{item.display}</NavLink>
-                </li>
-              ))}
+              {
+                nav__links.map((item,index)=>(
+                  <li className="nav__item" key={index}>
+                    <NavLink 
+                      to={item.path}
+                      className={navClass =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))
+              }
             </ul>
           </div>
-          {/* =========== menu end ============*/}
+          {/* =t=r=a=v=e=l= Menu End =t=o=u=r= */}
 
           <div className="nav__right d-flex align-items-center gap-4">
             <div className="nav__btns d-flex align-items-center gap-4">
-              <Button className="btn secondary__btn">
-                <Link to="/login">Login</Link>
-              </Button>
-              <Button className="btn primary__btn">
-                <Link to="/register">Register</Link>
-              </Button>
+
+              {
+                user?( 
+                <>
+                  <h5 className="mb-0 username" >{user.username}</h5>
+                  <Button className="btn btn-danger" onClick={logout}>Logout</Button>
+                </>
+                 ) : (
+                  <>
+                    <Button className="btn secondary__btn">
+                      <Link to='/login'>Login</Link>
+                    </Button>
+                    <Button className="btn primary__btn">
+                      <Link to='/register'>Register</Link>
+                    </Button>
+                  </>
+                 )
+              }
+
+              
             </div>
 
-            <span className="mobile__menu">
-              <i class="ri-menu-line"></i>
+            <span className="mobile__menu" onClick={toggleMenu}>
+            <i class="ri-menu-line"></i>
             </span>
           </div>
+
         </div>
       </Row>
     </Container>
   </header>
 }
 
-export default header
+export default Header
